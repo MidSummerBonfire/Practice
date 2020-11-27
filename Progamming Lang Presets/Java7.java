cb4e05d6db2c81c0eba7166c7dcdfd29c6534d75
MidSummerBonfire <74707470+MidSummerBonfire@users.noreply.github.com>
2020-11-27 06:05:12 -0800

    added Premade Templates
    HTML
    Java7

---
+++ Progamming Lang Presets/Java7.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Java7
+{
+    //functions go in here
+    //java.util.Scanner is used for input
+    /*public static void main(String[] args)
+    {
+        System.out.println("hello world");
+    }*/
+
+}
